d750a4aaef7107e8fd5bd8c61577de8afaa3aec5
Ambalembang <aldiAmbalembang@gmail.com>
2022-06-13 17:03:07 +0800

    membuat file koneksi, logout, register, session dan menghubungkan koneksi dan login

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php
+include "koneksi.php";
+error_reporting(0);
+session_start();
+
+
+if (isset($_POST['btnlogin'])) {
+	$username = $_POST['username'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: home.php");
+	} else {
+		echo "<script>alert('Woops! Email Atau Password anda Salah.')</script>";
+	}
+}
+
 ?>
 
 <!DOCTYPE html>
